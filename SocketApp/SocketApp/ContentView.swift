0f5b2e6ff141746abe6b99f641b72821b34ba34c
Austin Veal <aveal@uvm.edu>
2022-10-18 10:24:25 -0400

    Added basic create game functionality

--- SocketApp/SocketApp/ContentView.swift
+++ SocketApp/SocketApp/ContentView.swift
@@ -8,23 +8,55 @@
 import SwiftUI
 import SocketIO
 
+class formattedData: Identifiable {
+    var name = ""
+    var arrS: [String]?
+    var str: String?
+    var int: Int?
+    var intArr: [Int]?
+    var id = 0
+}
+
 final class Service: ObservableObject {
     private var manager = SocketManager(socketURL: URL(string: "ws://localhost:3000")!, config: [.log(true), .compress])
     
-    @Published var messages = [String]()
+    @Published var stringMessages = [String]()
+    @Published var stringArrayMessages = [[String]]()
+    @Published var intMessages = [Int]()
+    @Published var intArrayMessages = [[Int]]()
+    @Published var messages = [formattedData]()
     
     init() {
         let socket = manager.defaultSocket
         socket.on(clientEvent: .connect) { (data, ack) in
             print("Connected")
+            
+            /*
+             Sending messages to the server at port "NodeJS Server Port"
+             */
+            
             socket.emit("NodeJS Server Port", "Hi node.js server!")
+            socket.emit("Create Game", "New Poker Game")
         }
         
+        /*
+         The app or iOS client is listening on port "iOS Client Port"
+         */
         socket.on("iOS Client Port") { [weak self] (data, ack) in
             if let data = data[0] as? [String: String],
                let rawMessage = data["msg"]{
                 DispatchQueue.main.async {
-                    self?.messages.append(rawMessage)
+                    self?.stringMessages.append(rawMessage)
+                }
+            }
+        }
+        socket.on("Display Game") { [weak self] (data, ack) in
+            if let data = data[0] as? [String: [String]],
+               let rawmessage = data["msg"]{
+                DispatchQueue.main.async {
+                    var newData = formattedData()
+                    newData.arrS = rawmessage
+                    self?.messages.append(newData)
                 }
             }
         }
@@ -37,7 +69,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Recieved Messages from Node.js: ")
-            ForEach(service.messages, id: \.self) { msg in
+            
+            ForEach(service.stringMessages, id: \.self) { msg in
                 Text(msg).padding()
             }
             Spacer()
--- simpleServer/server.js
+++ simpleServer/server.js
@@ -5,6 +5,23 @@ var io = require('socket.io')(server);
 
 connections = [];
 
+/*
+ might want to create the object variable with
+ */
+
+
+/* maybe create a struct for each connection to be able to specify the ID of each socket to be able to reference each user*/
+
+gameCodeCounter = 1
+gameIdCounter = 0
+
+gameList = [];
+
+function Game(newId, users) {
+  gameId = newId;
+  gameUsers = users;
+}
+
 server.listen(process.env.PORT || 3000);
 console.log('Server is running...');
 
@@ -20,6 +37,30 @@ io.sockets.on('connection', function(socket) {
     
     socket.on('NodeJS Server Port', function(data) {
         console.log(data);
-        io.sockets.emit('iOS Client Port', {msg: 'Hi iOS Client!'});
+        io.sockets.emit('iOS Client Port', {msg: 'Hi iOS Client!'}, {msg1: ['Hello', 'World']});
+    });
+    
+    socket.on('Create Game', function(data) {
+        console.log(data);
+        
+        users = [];
+        users.push(socket.id);
+        
+        
+        newGame1 = {
+            code: gameCodeCounter,
+            gameId: gameIdCounter,
+            userList: users
+        }
+        
+        gameList.push(newGame1);
+        gameIdCounter += 1;
+        gameCodeCounter += 1;
+        
+        console.log(newGame1.code);
+        console.log(newGame1.gameId);
+        console.log(newGame1.userList);
+        
+        io.sockets.emit('Display Game', {msg: ['Hello', 'World']});
     });
 });
