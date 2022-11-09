5c534941e05b6e26651244b44701607727fcc857
ihseh <98051065+ihseh@users.noreply.github.com>
2022-11-08 22:29:27 -0500

    started playerHand function

--- pokerScript.js
+++ pokerScript.js
@@ -4,6 +4,17 @@ const Suit = {
     diamond: 2,
     heart: 3
 }
+const Rank = {
+    highCard: 0,
+    pair: 1,
+    twoPair: 2,
+    trips: 3,
+    straight: 4,
+    flush: 5,
+    fullHouse: 6,
+    quads: 7,
+    straightFlush: 8
+}
 
 class Card{
     constructor(num,suit){
@@ -40,11 +51,50 @@ class Deck{
     }
 }
 
+class PlayerHand{
+    constructor(){
+        this.hand = [];
+        this.rank = Rank.highCard;
+        //individual rank variables
+        //highcard:
+        this.fiveHighest = []
+        //pair:
+        this.firstPairRank = 0
+        this.threeOthers = []
+        //two pair:
+        this.secondPairRank = 0
+        this.twoPairKicker = 0
+        //trips:
+        this.tripsRank = 0
+        this.twoKickers = []
+        //straight:
+        var straightHigh = 0
+        //flush:
+        this.flushVals = []
+        //full house:
+        //set of 3 = tripsRank
+        this.twoSet = 0
+        //quads:
+        this.quadsRank = 0
+        this.quadsKicker = 0
+        //straight flush:
+        this.straightFlushHigh = 0
+    }
+    addCard(card){
+        this.hand.push(card);
+    }
+    //still needs getRank function and overloaded operators
+}
+
 let deck = new Deck();
 deck.shuffle();
 console.log(deck.cardDeck);
 console.log(deck.popTop())
 console.log(deck.cardDeck);
+let playerHand = new PlayerHand()
+playerHand.addCard(new Card(1,Suit.club))
+playerHand.addCard(new Card(13,Suit.diamond))
+console.log(playerHand)
 
 
 
