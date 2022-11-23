0fce476c84a8a4ae35a9f28d3b2c0c2390b0e582
Drew Smith <drewsmith1730@gmail.com>
2022-11-23 15:48:27 -0600

    reverted this file to only the ui for ease of server and client set up

--- SocketApp/SocketApp/ContentView.swift
+++ SocketApp/SocketApp/ContentView.swift
@@ -1,22 +1,12 @@
 //
 //  ContentView.swift
-//  SocketApp
+//  PokerSwiftUI
 //
-//  Created by Austin Veal on 10/17/22.
+//  Created by Matthew McCarthy on 10/19/22.
 //
 
 import SwiftUI
 import SocketIO
-
-class formattedData: Identifiable {
-    var name = ""
-    var arrS: [String]?
-    var str: String?
-    var int: Int?
-    var intArr: [Int]?
-    var id = 0
-}
-
 
 
 class DiffScreens: ObservableObject {
@@ -24,247 +14,36 @@ class DiffScreens: ObservableObject {
     @Published var home: Bool!
     @Published var createGame: Bool!
     @Published var joinGame: Bool!
+    @Published var waiting: Bool!
     @Published var inGame: Bool!
+    @Published var isHost: Bool!
+    
     
     init() {
         
         self.home = true
         self.createGame = false
         self.joinGame = false
+        self.waiting = false
         self.inGame = false
+        self.isHost = false
         
     }
     
     
-    
 }
 
-
-
-
-//might need a struct of card class
-//or I might be able to just say if it is this certain card
-
-final class Service: ObservableObject {
-    private var manager = SocketManager(socketURL: URL(string: "ws://localhost:3000")!, config: [.log(true), .compress])
+class Observables: ObservableObject {
     
-    @Published var stringMessages = [String]()
-    @Published var stringArrayMessages = [[String]]()
-    @Published var intMessages = [Int]()
-    @Published var intArrayMessages = [[Int]]()
-    @Published var messages = [formattedData]()
-    @Published var otherPlayers = [String]()
-    @Published var errorMessages = [String]()
-    @Published var gameDetails = [String: String]()
-    @Published var userCards = [String: String]()
-    @Published var cardsInMiddle = [String: String]()
-    //homescreen will be true by default
-
-    //These have to be published attributes so that they will change and update in real time
-    @Published var homeScreen = true
-    @Published var createGameScreen = false
-    @Published var joinGameScreen = false
-    @Published var gameScreen = false
-    @Published var playerTurn = false
-    @Published var firstRound = false
-    
+    @Published var host: Int!
     
     init() {
-        let socket = manager.defaultSocket
-        socket.on(clientEvent: .connect) { (data, ack) in
-            print("Connected")
-            
-            /*
-             Sending messages to the server at port "NodeJS Server Port"
-             */
-            
-            socket.emit("NodeJS Server Port", "Hi node.js server!")
-            socket.emit("Create Game", "New Poker Game")
-            /*
-            socket.emit("Join Game", )
-             */
-            socket.emit("Join Game", 1)
-        }
         
-        /*
-         The app or iOS client is listening on port "iOS Client Port"
-         */
-        socket.on("iOS Client Port") { [weak self] (data, ack) in
-            if let data = data[0] as? [String: String],
-               let rawMessage = data["msg"]{
-                DispatchQueue.main.async {
-                    self?.stringMessages.append(rawMessage)
-                }
-            }
-        }
+        self.host = 0
         
-        socket.on("Display Game") { [weak self] (data, ack) in
-            for x in 0...(data.count - 1){
-                if let data = data[x] as? [String: String]{
-                    if let rawMessage = data["game"]{
-                     DispatchQueue.main.async {
-                         self?.errorMessages.append(rawMessage)
-                     }
-                    }
-                    else if let rawMessage = data["buyin"]{
-                     DispatchQueue.main.async {
-                         self?.errorMessages.append(rawMessage)
-                     }
-                    }
-                }
-            }
-        }
-         
-        socket.on("Join Game Successful") { [weak self] (data, ack) in
-            if let data = data[0] as? [String: String],
-               let rawmessage = data["msg"]{
-                DispatchQueue.main.async {
-                    self?.stringMessages.append(rawmessage)
-                }
-            }
-        }
-        socket.on("Create Success") { [weak self] (data, ack) in
-            if let data = data[0] as? Int{
-                DispatchQueue.main.async {
-                    self?.stringMessages.append(String(data))
-                }
-            }
-        }
-        socket.on("New Player") { [weak self] (data, ack) in
-            if let data = data[0] as? [String: String],
-                let rawMessage = data["player"]{
-                 DispatchQueue.main.async {
-                     self?.otherPlayers.append(rawMessage)
-                 }
-             }
-            
-        }
-        
-        socket.on("Created Game") { [weak self] (data, ack) in
-            if let data = data[0] as? [String: String],
-                let rawMessage = data["msg"]{
-                 DispatchQueue.main.async {
-                     self?.otherPlayers.append(rawMessage)
-                 }
-             }
-            
-        }
-        
-        socket.on("Player Turn") { [weak self] (data, ack) in
-            //either display the user controls or just un grey them so they are able to be clicked
-            //display the user controls
-            //depending on what the user did send that information back to the server
-            self?.playerTurn = true
-            if let data = data[0] as? [String: String],
-                let rawMessage = data["msg"]{
-                if (rawMessage == "Round 0"){
-                    //ask for just the bet
-                    self?.firstRound = true
-                }
-                else{
-                    //not the first round
-                }
-                 DispatchQueue.main.async {
-                     self?.otherPlayers.append(rawMessage)
-                 }
-             }
-            
-            
-            //just an example but you get the point
-            socket.emit("Player Turn Done", "Fold")
-            
-        }
-        
-        socket.on("Player Added") { [weak self] (data, ack) in
-            //either display the user controls or just un grey them so they are able to be clicked
-            //display the user controls
-            //depending on what the user did send that information back to the server
-            self?.playerTurn = true
-            if let data = data[0] as? [String: String],
-                let rawMessage = data["msg"]{
-                 DispatchQueue.main.async {
-                     self?.stringMessages.append(rawMessage)
-                 }
-             }
-            
-            
-            //just an example but you get the point
-            socket.emit("Player Turn Done", "Fold")
-            
-        }
-        
-        socket.on("Start Game") { [weak self] (data, ack) in
-            //will need to now display the Game Screen
-            //will set the game screen to be gameScreen
-            //will set t
-            if let data = data[0] as? [String: String],
-                let rawMessage = data["msg"]{
-                 DispatchQueue.main.async {
-                     self?.otherPlayers.append(rawMessage)
-                 }
-             }
-            
-        }
-        
-        
-        socket.on("Join Game Invalid") { [weak self] (data, ack) in
-            if let data = data[0] as? [String: String],
-                let rawMessage = data["msg"]{
-                 DispatchQueue.main.async {
-                     self?.errorMessages.append(rawMessage)
-                 }
-             }
-            
-        }
-        
-        socket.connect()
-    }
-    
-    
-    func sendPlayerTurn(turn: [String:String]){
-        let socket = manager.defaultSocket
-        socket.connect()
-        socket.emit("Player Turn Done", turn)
-    }
-    
-    func startGame(turn: [String:String]){
-        let socket = manager.defaultSocket
-        socket.connect()
-        socket.emit("Start Game", turn)
-    }
-    
-    func addPlayer(){
-        let socket = manager.defaultSocket
-        socket.connect()
-        socket.emit("Add Player", "Player Joined")
-    }
-    
-    func createRound(){
-        let socket = manager.defaultSocket
-        socket.connect()
-        socket.emit("Start Round", "Create Round")
-    }
-    
-    func sendCreateGame(_ code: Int,_ name: String, _ stake: String){
-        //settings is a dictionary
-        //this is so that I am able to pass all of the settings with the key word being the setting
-        //ie pot size and the value being the value of the pot size
-        var settings: [String] = []
-        var codeString: String
-        
-        codeString = String(code)
-        
-        let socket = manager.defaultSocket
-        socket.connect()
-        settings.append(codeString)
-        settings.append(name)
-        settings.append(stake)
-        
-        socket.emit("Create Game", settings)
     }
     
 }
-
 
 struct ContentView: View {
     
@@ -283,6 +62,9 @@ struct ContentView: View {
         }
         if screens.joinGame == true {
             joinGameView()
+        }
+        if screens.waiting == true {
+            waitingView()
         }
         
         
@@ -369,8 +151,8 @@ struct AppHome: View {
 
 struct createGameView: View {
     
-    @ObservedObject var screens: DiffScreens = DiffScreens()
-    @ObservedObject var service = Service()
+    @EnvironmentObject var screens: DiffScreens
+    @EnvironmentObject var vars: Observables
     
     @State private var name: String = ""
     @State private var stake: String = ""
@@ -420,12 +202,15 @@ struct createGameView: View {
                 }
                 
                 Spacer()
-            
                 
                 Button(action: {
                     
                     //submit
-                    service.sendCreateGame(code, name, stake)
+                    
+                    self.screens.isHost = true
+                    self.screens.createGame = false
+                    self.screens.waiting = true
+                    
                     
                 }, label: {
                     Text("SUBMIT")
@@ -445,7 +230,8 @@ struct createGameView: View {
 
 struct joinGameView: View {
     
-    @ObservedObject var screens: DiffScreens = DiffScreens()
+    @EnvironmentObject var screens: DiffScreens
+    @EnvironmentObject var vars: Observables
     
     @State private var joinName: String = ""
     @State private var joinCode: String = ""
@@ -486,6 +272,8 @@ struct joinGameView: View {
                     //submit
                     
                     
+                    self.screens.joinGame = false
+                    self.screens.waiting = true
                     
                     
                 }, label: {
@@ -505,9 +293,74 @@ struct joinGameView: View {
     
 }
 
+struct waitingView: View {
+    
+    @EnvironmentObject var screens: DiffScreens
+    @EnvironmentObject var vars: Observables
+    
+    var body: some View{
+        
+        ZStack {
+            
+            Color.green
+                .ignoresSafeArea()
+            
+            // If user is host
+            if self.screens.isHost {
+                
+                VStack{
+                    
+                    Text("Waiting for host to start the game...")
+                        .font(.system(size: 30, weight: .bold, design: .monospaced))
+                    
+                    // Start game button
+                    Button(action: {
+                        
+                        //Start game action
+                        
+                        
+                        self.screens.waiting = false
+                        self.screens.inGame = true
+                        
+                        
+                    }, label: {
+                        Text("START GAME")
+                            .font(.system(size: 30, weight: .bold, design: .monospaced))
+
+                    })
+                    
+                    
+                    
+                    
+                }
+                
+                
+                
+            }
+            // If user is not host
+            if !self.screens.isHost {
+                
+                
+                VStack{
+                    
+                    Text("Waiting for host to start the game...")
+                        .font(.system(size: 30, weight: .bold, design: .monospaced))
+                    
+                }
+                
+            }
+        
+            
+        }
+        
+    }
+    
+}
+
+
 struct inGameView: View {
     
-    @ObservedObject var screens: DiffScreens = DiffScreens()
+    @EnvironmentObject var screens: DiffScreens
     
     var p1Name = "Player 1"
     var p2Name = "Player 2"
@@ -691,6 +544,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .previewInterfaceOrientation(.landscapeLeft)
+            .environmentObject(DiffScreens())
 
     }
 }
